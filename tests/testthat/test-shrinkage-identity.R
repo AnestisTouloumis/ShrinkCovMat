0b8465c757fb650e847ef19ee33a609ee19e1c15
AnestisTouloumis <A.Touloumis@brighton.ac.uk>
2019-05-30 11:42:07 +0100

    Adding code coverage.

--- tests/testthat/test-shrinkage-identity.R
+++ tests/testthat/test-shrinkage-identity.R
@@ -6,37 +6,36 @@ N <- 5
 datamat <- toeplitz(0.85^seq(0, p-1)) %*% matrix(rnorm(p*N), p, N)
 
 test_that("uncentered data", {
-  Target <- diag(p)
   SampleCov <- cov(t(datamat))
+  Y1N <- sum(diag(SampleCov))
   DataCentered <- datamat - rowMeans(datamat)
-  SigmaSample <- tcrossprod(DataCentered)/(N - 1)
-  SigmaSampleVariances <- diag(SigmaSample)
-  TraceSigmaHat <- sum(SigmaSampleVariances)
   Q <- sum(colSums(DataCentered^2)^2)/(N - 1)
-  TraceSigmaSquaredHat <- (N - 1)/(N * (N - 2) * (N - 3)) * ((N - 1) * (N - 2) * sum(SigmaSample^2) + (TraceSigmaHat)^2 - N * Q)
-  lambda <-  (TraceSigmaHat^2 + TraceSigmaSquaredHat)/ (N * TraceSigmaSquaredHat + TraceSigmaHat^2 - 2 * TraceSigmaHat * (N - 1) + p * (N - 1))
-  lambda <- max(0, min(lambda, 1))
+  Y2N <- (N - 1)/(N * (N - 2) * (N - 3)) * ((N - 1) * (N - 2) * sum(SampleCov^2) + (Y1N)^2 - N * Q)
+  lambdahat <-  (Y1N^2 + Y2N)/ (N * Y2N + Y1N^2 - 2 * Y1N * (N - 1) + p * (N - 1))
+  lambdahat <- max(0, min(lambdahat, 1))
+  Target <- diag(p)
   x <- shrinkcovmat.identity(datamat)
-  expect_equal(x$Sigmahat, (1-lambda) * SampleCov + lambda * Target)
-  expect_equal(x$lambdahat, lambda)
+  expect_equal(x$Sigmahat, (1 - lambdahat) * SampleCov + lambdahat * Target)
+  expect_equal(x$lambdahat, lambdahat)
   expect_equal(x$Sigmasample, SampleCov)
   expect_equal(x$Target, Target)
 })
 
 
 test_that("centered data", {
-  SigmaSample <- tcrossprod(datamat)/N
-  TraceSigmaHat <- sum(diag(SigmaSample))
-  TraceSigmaSquaredHat <- 0
-  for (i in 1:(N - 1)) TraceSigmaSquaredHat <- sum(crossprod(datamat[, i], datamat[, (i + 1):N])^2) + TraceSigmaSquaredHat
-  TraceSigmaSquaredHat <- 2 * TraceSigmaSquaredHat/N/(N - 1)
-  lambda <- (TraceSigmaHat^2 + TraceSigmaSquaredHat)/((N + 1) * TraceSigmaSquaredHat + TraceSigmaHat^2 - 2 * TraceSigmaHat * N + p * N)
-  lambda <- max(0, min(lambda, 1))
-  Target <- diag(p)
   SampleCov <- tcrossprod(datamat)/N
+  Y1N <- sum(diag(SampleCov))
+  Y2N <- 0
+  for (i in 1:(N - 1)) {
+    Y2N <- sum(crossprod(datamat[, i], datamat[, (i + 1):N])^2) + Y2N
+  }
+  Y2N <- 2 * Y2N/N/(N - 1)
+  lambdahat <- (Y1N^2 + Y2N)/((N + 1) * Y2N + Y1N^2 - 2 * Y1N * N + p * N)
+  lambdahat <- max(0, min(lambdahat, 1))
+  Target <- diag(p)
   y <- shrinkcovmat.identity(datamat, centered = TRUE)
-  expect_equal(y$Sigmahat, (1-lambda) * SampleCov + lambda * Target)
-  expect_equal(y$lambdahat, lambda)
+  expect_equal(y$Sigmahat, (1-lambdahat) * SampleCov + lambdahat * Target)
+  expect_equal(y$lambdahat, lambdahat)
   expect_equal(y$Sigmasample, SampleCov)
   expect_equal(y$Target, Target)
 })
--- tests/testthat/test-shrinkage-unequal.R
+++ tests/testthat/test-shrinkage-unequal.R
@@ -7,12 +7,13 @@ datamat <- toeplitz(0.85^seq(0, p-1)) %*% matrix(rnorm(p*N), p, N)
 
 
 test_that("uncentered data", {
+  SampleCov <- cov(t(datamat))
   DataCentered <- datamat - rowMeans(datamat)
-  SigmaSample <- tcrossprod(DataCentered)/(N - 1)
-  SigmaSampleVariances <- diag(SigmaSample)
-  TraceSigmaHat <- sum(SigmaSampleVariances)
+  SigmaSampleVariances <- diag(SampleCov)
+  Y1N <- sum(SigmaSampleVariances)
+  DataCentered <- datamat - rowMeans(datamat)
   Q <- sum(colSums(DataCentered^2)^2)/(N - 1)
-  TraceSigmaSquaredHat <- (N - 1)/(N * (N - 2) * (N - 3)) * ((N - 1) * (N - 2) * sum(SigmaSample^2) + (TraceSigmaHat)^2 - N * Q)
+  Y2N <- (N - 1)/(N * (N - 2) * (N - 3)) * ((N - 1) * (N - 2) * sum(SampleCov^2) + (Y1N)^2 - N * Q)
   Sum1 <- Sum21 <- Sum22 <- Sum3 <- rep(0, p)
   for (i in 1:(N - 1)) {
     data2 <- matrix(datamat[, (i + 1):N], p, N - i)
@@ -26,14 +27,13 @@ test_that("uncentered data", {
   Term3 <- 4 * (sum(Sum1^2) - sum(Sum3) - Term2)
   Term2 <- Term2/N/(N - 1)/(N - 2)
   Term3 <- Term3/N/(N - 1)/(N - 2)/(N - 3)
-  TraceDiagonalSigmaSquaredHat <- Term1 - 2 * Term2 + Term3
-  lambda <- (TraceSigmaHat^2 + TraceSigmaSquaredHat - 2 * TraceDiagonalSigmaSquaredHat)/(N * TraceSigmaSquaredHat + TraceSigmaHat^2 - (N + 1) * TraceDiagonalSigmaSquaredHat)
-  lambda <- max(0, min(lambda, 1))
+  Y3N <- Term1 - 2 * Term2 + Term3
+  lambdahat <- (Y1N^2 + Y2N - 2 * Y3N)/(N * Y2N + Y1N^2 - (N + 1) * Y3N)
+  lambdahat <- max(0, min(lambdahat, 1))
   x <- shrinkcovmat.unequal(datamat)
-  SampleCov <- cov(t(datamat))
-  Target <- diag(diag(SampleCov), p)
-  expect_equal(x$Sigmahat, (1-lambda) * SampleCov + lambda * Target)
-  expect_equal(x$lambdahat, lambda)
+  Target <- diag(SigmaSampleVariances, p)
+  expect_equal(x$Sigmahat, (1-lambdahat) * SampleCov + lambdahat * Target)
+  expect_equal(x$lambdahat, lambdahat)
   expect_equal(x$Sigmasample, SampleCov)
   expect_equal(x$Target, Target)
 })
@@ -41,23 +41,22 @@ test_that("uncentered data", {
 
 
 test_that("centered data", {
-  SigmaSample <- tcrossprod(datamat)/N
-  TraceSigmaHat <- sum(diag(SigmaSample))
-  TraceSigmaSquaredHat <- TraceDiagonalSigmaSquaredHat <- 0
+  SigmaCov <- tcrossprod(datamat)/N
+  SigmaSampleVariances <- diag(SigmaCov)
+  Y1N <- sum(SigmaSampleVariances)
+  Y2N <- Y3N <- 0
   for (i in 1:(N - 1)) {
-    TraceSigmaSquaredHat <- sum(crossprod(datamat[, i], datamat[, (i + 
-                                                               1):N])^2) + TraceSigmaSquaredHat
-    TraceDiagonalSigmaSquaredHat <- sum((datamat[, i] * datamat[, (i + 
-                                                               1):N])^2) + TraceDiagonalSigmaSquaredHat
+    Y2N <- sum(crossprod(datamat[, i], datamat[, (i + 1):N])^2) + Y2N
+    Y3N <- sum((datamat[, i] * datamat[, (i + 1):N])^2) + Y3N
   }
-  TraceSigmaSquaredHat <- 2 * TraceSigmaSquaredHat/N/(N - 1)
-  TraceDiagonalSigmaSquaredHat <- 2 * TraceDiagonalSigmaSquaredHat/(N*(N - 1))
-  lambda <- (TraceSigmaHat^2 + TraceSigmaSquaredHat -  2 * TraceDiagonalSigmaSquaredHat)/ ((N + 1) * TraceSigmaSquaredHat + TraceSigmaHat^2 - (N + 2) * TraceDiagonalSigmaSquaredHat)
-  lambda <- max(0, min(lambda, 1))
+  Y2N <- 2 * Y2N/N/(N - 1)
+  Y3N <- 2 * Y3N/N/(N - 1)
+  lambdahat <- (Y1N^2 + Y2N -  2 * Y3N)/ ((N + 1) * Y2N + Y1N^2 - (N + 2) * Y3N)
+  lambdahat <- max(0, min(lambdahat, 1))
   y <- shrinkcovmat.unequal(datamat, centered = TRUE)
-  Target <- diag(diag(SigmaSample), p)
-  expect_equal(y$Sigmahat, (1-lambda) * SigmaSample + lambda * Target)
-  expect_equal(y$lambdahat, lambda)
-  expect_equal(y$Sigmasample, SigmaSample)
+  Target <- diag(SigmaSampleVariances, p)
+  expect_equal(y$Sigmahat, (1 - lambdahat) * SigmaCov + lambdahat * Target)
+  expect_equal(y$lambdahat, lambdahat)
+  expect_equal(y$Sigmasample, SigmaCov)
   expect_equal(y$Target, Target)
 })
