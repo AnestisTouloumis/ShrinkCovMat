f3de69007d254fae8ae28c37080950da8a8dfd22
Anestis Touloumis <A.Touloumis@brighton.ac.uk>
2022-09-16 23:43:02 +0100

    220916: fixed the argument target in shrinkcovmat

--- R/shrinkcovmat.R
+++ R/shrinkcovmat.R
@@ -9,7 +9,7 @@
 #' @param data a numeric matrix containing the data.
 #' @param centered a logical indicating if the mean vector is the zero vector.
 #' @param target a character indicating the target matrix. Options include 
-#' 'spherical', 'identity' or 'diagonal'. 
+#' 'sphericity', 'identity' or 'diagonality'. 
 #' @return Returns an object of the class 'shrinkcovmathat' that has
 #' components: \item{Sigmahat}{The Stein-type shrinkage estimator of the
 #' covariance matrix.} \item{lambdahat}{The estimated optimal shrinkage
@@ -17,8 +17,7 @@
 #' \item{Target}{The target covariance matrix.} \item{centered}{If the data are
 #' centered around their mean vector.}
 #' @author Anestis Touloumis
-#' @seealso \code{\link{shrinkcovmat.equal}}, \code{\link{shrinkcovmat.identity}}
-#' and \code{\link{shrinkcovmat.unequal}}.
+#' @seealso \code{\link{targetselection}}.
 #' @references Touloumis, A. (2015) nonparametric Stein-type Shrinkage
 #' Covariance Matrix Estimators in High-Dimensional Settings.
 #' \emph{Computational Statistics & Data Analysis} \bold{83}, 251--261.
@@ -26,15 +25,21 @@
 #' data(colon)
 #' normal_group <- colon[, 1:40]
 #' tumor_group <- colon[, 41:62]
-#' sigma_hat_normal_group <- shrinkcovmat.identity(normal_group)
+#' targetselection(normal_group)
+#' sigma_hat_normal_group <- shrinkcovmat(normal_group)
 #' sigma_hat_normal_group
-#' sigma_hat_tumor_group <- shrinkcovmat.identity(tumor_group)
+#' targetselection(normal_group)
+#' sigma_hat_tumor_group <- shrinkcovmat(tumor_group)
 #' sigma_hat_tumor_group
 #' @export
-shrinkcovmat <- function(data, centered = FALSE, target = "spherical") { # nolint
-  if (target == "spherical") {
+shrinkcovmat <- function(data, centered = FALSE, target = "sphericity") { # nolint
+  targets <- c("sphericity", "identity", "diagonality")
+  if (!is.element(target, targets)) {
+    stop("'target' must be 'sphericity', 'identity' or 'diagonality'")
+    }
+  if (target == "sphericity") {
     ans <- shrinkcovmat.equal(data, centered)
-    } else if (target == "diagonal") {
+    } else if (target == "diagonality") {
       ans <- shrinkcovmat.unequal(data, centered)
       } else {
         ans <- shrinkcovmat.identity(data, centered)
