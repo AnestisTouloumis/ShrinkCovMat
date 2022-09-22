a17785beb32e69beef79a1b4f06a57401338412b
Anestis Touloumis <A.Touloumis@brighton.ac.uk>
2022-09-23 00:01:57 +0100

    220923: updated internal functions

--- R/ShrinkCovMat_internals.R
+++ R/ShrinkCovMat_internals.R
@@ -1,29 +1,16 @@
 shrinkcovmat_unequal <- function(data, centered, p, n) {
-  if (!centered) {
-    sample_covariance_matrix <- cov(t(data))
-    sample_variances <- apply(data, 1, var)
-    trace_statistics <- trace_stats_uncentered(data) # nolint
-    trace_sigma_hat <- trace_statistics[1]
-    trace_sigma_squared_hat <- trace_statistics[2]
-    trace_diagonal_sigma_sq_hat <- trace_statistics[3]
-    lambda_hat <- (trace_sigma_hat^2 + trace_sigma_squared_hat -
-      (2 - 2 / n) * trace_diagonal_sigma_sq_hat) /
-      (n * trace_sigma_squared_hat + trace_sigma_hat^2 -
-        (n + 1 - 2 / n) * trace_diagonal_sigma_sq_hat)
-    lambda_hat <- max(0, min(lambda_hat, 1))
-  } else {
-    sample_covariance_matrix <- tcrossprod(data) / n
-    sample_variances <- apply(data, 1, function(x) mean(x^2))
-    trace_statistics <- trace_stats_centered(data) # nolintr
-    trace_sigma_hat <- trace_statistics[1]
-    trace_sigma_squared_hat <- trace_statistics[2]
-    trace_diagonal_sigma_sq_hat <- trace_statistics[3]
-    lambda_hat <- (trace_sigma_hat^2 + trace_sigma_squared_hat -
-      (2 - 2 / (n + 1)) * trace_diagonal_sigma_sq_hat) /
-      ((n + 1) * trace_sigma_squared_hat + trace_sigma_hat^2 -
-        (n + 2 - 2 / (n + 1)) * trace_diagonal_sigma_sq_hat)
-    lambda_hat <- max(0, min(lambda_hat, 1))
-  }
+  sample_covariance_matrix <- calculate_covariance_matrix(data, centered, n)
+  trace_statistics <- calculate_trace_statistic(data, centered)
+  trace_sigma_hat <- trace_statistics[1]
+  trace_sigma_squared_hat <- trace_statistics[2]
+  trace_diagonal_sigma_sq_hat <- trace_statistics[3]
+  sample_variances <- calculate_sample_variances(data, centered)
+  sample_size <- ifelse(centered, n + 1, n)
+  lambda_hat <- (trace_sigma_hat^2 + trace_sigma_squared_hat -
+    (2 - 2 / sample_size) * trace_diagonal_sigma_sq_hat) /
+    (sample_size * trace_sigma_squared_hat + trace_sigma_hat^2 -
+      (sample_size + 1 - 2 / sample_size) * trace_diagonal_sigma_sq_hat)
+  lambda_hat <- max(0, min(lambda_hat, 1))
   if (lambda_hat < 1) {
     sigma_hat <- (1 - lambda_hat) * sample_covariance_matrix +
       diag(lambda_hat * sample_variances, p)
@@ -42,25 +29,15 @@ shrinkcovmat_unequal <- function(data, centered, p, n) {
 
 
 shrinkcovmat_equal <- function(data, centered, p, n) {
-  if (!centered) {
-    sample_covariance_matrix <- cov(t(data))
-    trace_statistics <- trace_stats_uncentered(data) # nolintr
-    trace_sigma_hat <- trace_statistics[1]
-    nu_hat <- trace_sigma_hat / p
-    trace_sigma_squared_hat <- trace_statistics[2]
-    lambda_hat <- (trace_sigma_hat^2 + trace_sigma_squared_hat) /
-      (n * trace_sigma_squared_hat + (p - n + 1) / p * trace_sigma_hat^2)
-    lambda_hat <- min(lambda_hat, 1)
-  } else {
-    sample_covariance_matrix <- tcrossprod(data) / n
-    trace_statistics <- trace_stats_centered(data) # nolintr
-    trace_sigma_hat <- trace_statistics[1]
-    nu_hat <- trace_sigma_hat / p
-    trace_sigma_squared_hat <- trace_statistics[2]
-    lambda_hat <- (trace_sigma_hat^2 + trace_sigma_squared_hat) /
-      ((n + 1) * trace_sigma_squared_hat + (p - n) / p * trace_sigma_hat^2)
-    lambda_hat <- min(lambda_hat, 1)
-  }
+  sample_covariance_matrix <- calculate_covariance_matrix(data, centered, n)
+  trace_statistics <- calculate_trace_statistic(data, centered)
+  trace_sigma_hat <- trace_statistics[1]
+  trace_sigma_squared_hat <- trace_statistics[2]
+  sample_size <- ifelse(centered, n + 1, n)
+  nu_hat <- trace_sigma_hat / p
+  lambda_hat <- (trace_sigma_hat^2 + trace_sigma_squared_hat) /
+    (sample_size * trace_sigma_squared_hat + (p - sample_size + 1) / p * trace_sigma_hat^2)
+  lambda_hat <- min(lambda_hat, 1)
   if (lambda_hat < 1) {
     sigmahat <- (1 - lambda_hat) * sample_covariance_matrix +
       diag(nu_hat * lambda_hat, p)
@@ -79,25 +56,15 @@ shrinkcovmat_equal <- function(data, centered, p, n) {
 
 
 shrinkcovmat_identity <- function(data, centered, p, n) {
-  if (!centered) {
-    sample_covariance_matrix <- cov(t(data))
-    trace_statistics <- trace_stats_uncentered(data) # nolintr
-    trace_sigma_hat <- trace_statistics[1]
-    trace_sigma_squared_hat <- trace_statistics[2]
-    lambda_hat <- (trace_sigma_hat^2 + trace_sigma_squared_hat) /
-      (n * trace_sigma_squared_hat + trace_sigma_hat^2 -
-        2 * trace_sigma_hat * (n - 1) + p * (n - 1))
-    lambda_hat <- max(0, min(lambda_hat, 1))
-  } else {
-    sample_covariance_matrix <- tcrossprod(data) / n
-    trace_statistics <- trace_stats_centered(data) # nolintr
-    trace_sigma_hat <- trace_statistics[1]
-    trace_sigma_squared_hat <- trace_statistics[2]
-    lambda_hat <- (trace_sigma_hat^2 + trace_sigma_squared_hat) /
-      ((n + 1) * trace_sigma_squared_hat + trace_sigma_hat^2 -
-        2 * trace_sigma_hat * n + p * n)
-    lambda_hat <- max(0, min(lambda_hat, 1))
-  }
+  sample_covariance_matrix <- calculate_covariance_matrix(data, centered, n)
+  trace_statistics <- calculate_trace_statistic(data, centered)
+  trace_sigma_hat <- trace_statistics[1]
+  trace_sigma_squared_hat <- trace_statistics[2]
+  sample_size <- ifelse(centered, n + 1, n)
+  lambda_hat <- (trace_sigma_hat^2 + trace_sigma_squared_hat) /
+    (sample_size * trace_sigma_squared_hat + trace_sigma_hat^2 -
+      2 * trace_sigma_hat * (sample_size - 1) + p * (sample_size - 1))
+  lambda_hat <- max(0, min(lambda_hat, 1))
   if (lambda_hat < 1) {
     sigma_hat <- (1 - lambda_hat) * sample_covariance_matrix +
       diag(lambda_hat, p)
@@ -111,5 +78,35 @@ shrinkcovmat_identity <- function(data, centered, p, n) {
     centered = centered
   )
   class(ans) <- "shrinkcovmathat"
+  ans
+}
+
+
+calculate_covariance_matrix <- function(data, centered, n) {
+  if (centered) {
+    ans <- tcrossprod(data) / n
+  } else {
+    ans <- cov(t(data))
+  }
+  ans
+}
+
+
+calculate_trace_statistic <- function(data, centered) {
+  if (centered) {
+    ans <- trace_stats_centered(data)
+  } else {
+    ans <- trace_stats_uncentered(data)
+  }
+  ans
+}
+
+
+calculate_sample_variances <- function(data, centered) {
+  if (centered) {
+    ans <- apply(data, 1, function(x) mean(x^2))
+  } else {
+    ans <- apply(data, 1, var)
+  }
   ans
 }
